2449a18997fff498a71a7870f5c17eeb26abbf65
vipin <vipinsahuji123@gmail.com>
2025-04-20 22:08:03 +0530

    remove navbar extra part

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,18 +6,6 @@ const Navbar = () => {
       <nav className="bg-red-100 p-3 ">
         <div className="mx-auto max-w-7xl flex justify-between items-center">
           <a href="#" className="text-2xl font-bold">TO-DO LIST</a>
-
-          <ul className="flex space-x-15  ">
-            <a href="#" className=" hover:text-gray-600">
-              Home
-            </a>
-            <a href="#" className=" hover:text-gray-600">
-              About
-            </a>
-            <a href="#" className=" hover:text-gray-600">
-              Contact
-            </a>
-          </ul>
         </div>
       </nav>
     </>
